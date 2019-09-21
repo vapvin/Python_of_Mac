a49a4eec38d89c1c39b95f6054ece78760e1a738
vapvin <tpehdj12@daum.net>
2019-09-22 01:06:25 +0900

    why don't write Korean String

---
+++ py/001.py
@@ -0,0 +1,20 @@
+# -*-Encoding: UTF-8 -*-
+import sys
+
+reload(sys)
+
+sys.setdefaultencoding('utf-8')
+
+
+
+print("Hello World")
+print("Mary's cosmetics")
+print("신씨가 소리질렀다. \"도둑이야\".")
+print(" \"C:\\Windows\" ") 
+
+# \n 은 줄바꿈 \t 은 들여쓰기
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+# 연결되어 출력 사이에 공백
+print("오늘은","일요일")
+
