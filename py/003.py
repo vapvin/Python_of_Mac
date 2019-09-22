4aff56d285260a1f062ddfb6abb443006f81ae78
vapvin <tpehdj12@daum.net>
2019-09-22 20:57:04 +0900

    study of Sunday

---
+++ py/003.py
@@ -0,0 +1,12 @@
+# -*-Encoding: UTF-8 -*-
+
+# name = input('What is your Name?')
+# print('{}, Hi'.format(name))
+
+my_bro = {
+    1: "min",
+    2: 15,
+    3: "male"
+}
+my_bro[4] = 'stu'
+print(my_bro)
